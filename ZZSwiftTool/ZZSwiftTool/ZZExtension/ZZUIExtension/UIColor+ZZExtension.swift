01d32952047bbf6e6d21ee3af03ad518ee39880f
lixiangzhou <lixiangzhouno1@sina.com>
2017-03-17 18:13:18 +0800

    添加16进制颜色值生成Color对象

--- ZZSwiftTool/ZZSwiftTool/ZZExtension/ZZUIExtension/UIColor+ZZExtension.swift
+++ ZZSwiftTool/ZZSwiftTool/ZZExtension/ZZUIExtension/UIColor+ZZExtension.swift
@@ -15,10 +15,18 @@ extension UIColor {
     /// - parameter red:   红
     /// - parameter green: 绿
     /// - parameter blue:  蓝
-    ///
-    /// - returns: 生成的颜色
+    /// - parameter alpha: 透明度
     convenience init(red: Int, green: Int, blue: Int) {
         self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1)
+    }
+    
+    
+    /// 16进制rgb颜色值生成对应UIColor
+    ///
+    /// - parameter rgbValue: rgb颜色值
+    /// - parameter alpha:    透明度
+    convenience init(rgbValue: Int, alpha: Int = 255) {
+        self.init(red: CGFloat((rgbValue & 0xff0000) >> 16) / 255.0, green: CGFloat((rgbValue & 0x00ff00) >> 8) / 255.0, blue: CGFloat((rgbValue & 0x0000ff) >> 16) / 255.0, alpha: CGFloat(alpha & 0xff) / 255.0)
     }
     
     /// 随机色
